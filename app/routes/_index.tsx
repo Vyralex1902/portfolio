eceedc61ab15997d893f4281f950745b9688e835
“cybersparkx” <lucammontanari@gmail.com>
2024-06-18 18:22:10 +0200

    update good

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useEffect, useState } from "react";
 
+import "../styles/home.css";
+
 import mypic1 from "../assets/mypic1.jpg"
+import backgroundImg1 from "../assets/mainPageBackground.jpg"
 
 export const meta: MetaFunction = () => {
   return [
@@ -29,9 +32,34 @@ export default function Index() {
 
   return (
     <div style={{ width: "100%", height: "100%" }} className="flex flex-col justify-center items-center align-top pt-20 bg-white dark:bg-[#1E1E1E]">
-      <div id="topdiv" className="flex flex-col justify-center items-center">
-        <h1 id="title" className="darkmode-text-h2">Luca's Portfolio</h1>
-        <img src={mypic1} className="w-24 h-24 rounded-full border-2 border-gray-600"></img>
+      <img className="z-0 absolute top-0 h-[100vh] w-full" src={backgroundImg1}></img>
+      <div id="topdiv" className="z-10 flex flex-col justify-center items-center">
+        <h1 id="title" className="darkmode-text-h2 animate-slideInFromTop">Luca's Portfolio</h1>
+        <img src={mypic1} className="w-24 h-24 rounded-full border-2 border-gray-600 animate-reveal"></img>
+      </div>
+      <div id="middleHeading" className="z-10 bg-gray-400 bg-opacity-30 mt-5 flex flex-row text-black dark:text-white w-[75vh] h-[55px] backdrop-blur-lg border-1 border-gray-300
+      rounded-md text-center items-center justify-center align-middle">
+        <div className="flex flex-row gap-2 text-gray-600">
+          <a href="/portfolio/programming">
+            <h3 className="hover:text-green-700">Programming</h3>
+          </a>
+          <p>|</p>
+          <a href="/portfolio/hobbies">
+            <h3 className="hover:text-green-700">Hobbies</h3>
+          </a>
+          <p>|</p>
+          <a href="/portfolio/mystory">
+            <h3 className="hover:text-green-700">My Story</h3>
+          </a>
+          <p>|</p>
+          <a href="/portfolio/work-now">
+            <h3 className="hover:text-green-700">Current Occupation</h3>
+          </a>
+        </div>
+      </div>
+      <div className="z-10 absolute bottom-0 flex flex-row text-center text-black dark:text-white">
+        <h1 id="quote">"Living has to be difficult to be called living."</h1>
+        {/* <p className="z-10 text-black dark:text-white text-3xl">- Luca M. 2024</p> */}
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideInFromTop: {
+          '0%': { transform: 'translateY(-100px)' },
+          '100%': { transform: 'translateY(0px)' },
+        },
+        reveal: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        slideInFromTop: 'slideInFromTop 1s',
+        reveal: 'reveal 2s ease-out',
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
