8acc52b89c53e82a44874f3ce3f346ce424c3bc4
“cybersparkx” <lucammontanari@gmail.com>
2024-06-26 09:45:47 +0200

    home redesign

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,8 +6,8 @@ import "../styles/home.css";
 
 import mypic2 from "../assets/mypic2.jpeg"
 import mypic3 from "../assets/mypic3.png"
-import backgroundImg1 from "../assets/mainPageBackground.jpg"
 import backgroundImg2 from "../assets/mainPageBackground2.jpg"
+import mainPageTextClip from "../assets/mainPageTextClip.jpg"
 import separatorimg from "../assets/separatorimg.jpg"
 import macFrame from "../assets/mac-frame.png"
 import tertitimg from "../assets/terminaltitle.png"
@@ -51,11 +51,6 @@ export default function Index() {
     <div style={{ width: "100%", height: "100%" }} className="fjc flex-col items-center align-top pt-24">
       <input type="text" className="absolute hidden" id="pageName" value={"home"} readOnly></input>
       <img className="z-0 fixed top-0 h-[100vh] w-full" src={backgroundImg2}></img>
-      {/* <img className="z-0 absolute w-[140vh] mt-[70px] opacity-75" src={macFrame}></img> */}
-      <div id="topdiv" className="z-10 flex flex-col justify-center items-center">
-        <h1 id="title" className="text-white animate-slideInFromTop">Luca's Portfolio</h1>
-        <img id="profilepic1" className="w-24 h-24 rounded-full border-2 border-gray-600 animate-reveal"></img>
-      </div>
       <div id="middleHeading" className="z-10 bg-white bg-opacity-20 mt-5 fjc flex-row text-white w-[50vw] h-[55px] backdrop-blur-lg
       rounded-md text-center items-center align-middle">
         <div className="flex flex-row gap-2 text-gray-600">
@@ -76,38 +71,46 @@ export default function Index() {
         <h2>I had started programming at the age of 13. I started with VB.NET, then I migrated to...</h2>
         <a href="/portfolio/mystory"><p className="underline text-blue-600">Continue reading</p></a>
       </div>
-      <div className="flex flex-row gap-8 my-5">
-        <div className='' id='downloadbtncontainer'>
-          <div className='flex justify-center pt-2'>
-            <div className='w-fit'>
-              <a href='/portfolio/hireme'>
-                <div className="downloadbtn_div">
-                  <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
-                    <div className='flex flex-col justify-center items-center'>
-                      <p className="text-blue-600">Hire Me</p>
-                    </div>
-                  </motion.button>
-                </div>
-              </a>
+      <div id="topdiv" className="z-10 flex flex-col items-center align-top">
+        <div className="z-10 flex flex-row items-center align-top">
+          <h1 id="gigatitle" className="text-white animate-slideInFromTop">LUCA MONTANARI</h1><p id="gigatitlesub" className="text-5xl text-white animate-slideInFromTop">'S</p>
+        </div>
+        <h1 className="text-5xl text-white -mt-5 mb-5">Portfolio</h1>
+        <div className="z-10 flex align-middle items-center gap-5 mb-20">
+          <div className='' id='downloadbtncontainer'>
+            <div className='flex justify-center pt-2'>
+              <div className='w-fit'>
+                <a href='/portfolio/hireme'>
+                  <div className="downloadbtn_div">
+                    <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
+                      <div className='flex flex-col justify-center items-center'>
+                        <p className="text-blue-600">Hire Me</p>
+                      </div>
+                    </motion.button>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='' id='downloadbtncontainer'>
-          <div className='flex justify-center pt-2'>
-            <div className='w-fit'>
-              <a href='/portfolio/contactme'>
-                <div className="downloadbtn_div">
-                  <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
-                    <div className='flex flex-col justify-center items-center'>
-                      <p className="text-blue-600">Contact Me</p>
-                    </div>
-                  </motion.button>
-                </div>
-              </a>
+          <img id="profilepic1" className="w-24 h-24 rounded-full border-2 border-gray-600 animate-reveal"></img>
+          <div className='' id='downloadbtncontainer'>
+            <div className='flex justify-center pt-2'>
+              <div className='w-fit'>
+                <a href='/portfolio/contactme'>
+                  <div className="downloadbtn_div">
+                    <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
+                      <div className='flex flex-col justify-center items-center'>
+                        <p className="text-blue-600">Contact Me</p>
+                      </div>
+                    </motion.button>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <img className="z-20 w-[500px]" src={tertitimg}></img>
       <div id="myinfodiv" className="z-10 -mt-2 flex flex-col justify-center items-center align-top w-[499px] bg-black bg-opacity-40 backdrop-blur-lg p-3
        text-gray-300">
