b81212bbe3325b771b788b47e12246b9d4095443
“cybersparkx” <lucammontanari@gmail.com>
2024-06-27 21:25:59 +0200

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -67,144 +67,189 @@ export default function Index() {
     else {
       document.getElementById("MobileAlert")?.classList.add("hidden");
     }
-  });
 
-  return (
-    <div id="fulldiv" style={{ width: "100%", height: "100%" }} className="fjc flex-col items-center align-top pt-24 bg-black">
-      <input type="text" className="absolute hidden" id="pageName" value={"home"} readOnly></input>
-      <img className="z-0 fixed top-0 h-[100vh] w-full opacity-80 blur-[1px]" src={backgroundImg2}></img>
-      <div id="MobileAlert" className="absolute top-[75px] max-w-[90vw] text-yellow-400"><p>It looks like you're on mobile! I firmly advise you to use the destkop view or just use a PC.</p></div>
-      <div id="middleHeading" className="z-10 bg-white bg-opacity-20 mt-5 fjc flex-row text-white w-[35vw] h-[55px] backdrop-blur-lg
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    //   document.getElementById("glitchText")!.onmouseover = event => {
+    //     setInterval(() => {
+    //       document.getElementById("glitchText")!.innerText.split('')
+    //         .map((letter, index) => {
+    //           if (index < 1) {
+    //             return document.getElementById("glitchText")!.getAttribute('value');
+    //           }
+    //                   .map(letter => letters[Math.floor(Math.random() * 26)]).join('');
+    //         })
+    //     }, 30);
+    //   }
+    // });
+
+    return (
+      <div id="fulldiv" style={{ width: "100%", height: "100%" }} className="fjc flex-col items-center align-top pt-24 bg-black">
+        <input type="text" className="absolute hidden" id="pageName" value={"home"} readOnly></input>
+        <img className="z-0 fixed top-0 h-[100vh] w-full opacity-80 blur-[1px]" src={backgroundImg2}></img>
+        <div id="MobileAlert" className="absolute top-[75px] max-w-[90vw] text-yellow-400"><p>It looks like you're on mobile! I firmly advise you to use the destkop view or just use a PC.</p></div>
+        <div id="middleHeading" className="z-10 bg-white bg-opacity-20 mt-5 fjc flex-row text-white w-[35vw] h-[55px] backdrop-blur-lg
       rounded-md text-center items-center align-middle">
-        <div className="flex flex-row gap-2 text-gray-600">
-          <Link to="programming" className="cursor-context-menu">
-            <h3 className="hover:text-green-700">Coding Knowledge</h3>
-          </Link>
-          <p>|</p>
-          <Link to="hobbies" className="cursor-context-menu">
-            <h3 className="hover:text-green-700">Passions</h3>
-          </Link>
-          <p>|</p>
+          <div className="flex flex-row gap-2 text-gray-600">
+            <Link to="programming" className="cursor-context-menu">
+              <h3 className="hover:text-green-700">Coding Knowledge</h3>
+            </Link>
+            <p>|</p>
+            <Link to="hobbies" className="cursor-context-menu">
+              <h3 className="hover:text-green-700">Passions</h3>
+            </Link>
+            <p>|</p>
+            <Link to="mystory" className="cursor-context-menu">
+              <h3 className="hover:text-green-700">My Story</h3>
+            </Link>
+          </div>
+        </div>
+        <div id="middleStory" className="z-10 flex flex-col justify-center items-center align-top max-w-[25vw] bg-white bg-opacity-20 backdrop-blur-lg p-3 text-gray-600">
+          <h2>I had started programming at the age of 13. I started with VB.NET, then I migrated to...</h2>
           <Link to="mystory" className="cursor-context-menu">
-            <h3 className="hover:text-green-700">My Story</h3>
+            <p className="underline text-blue-600">Continue reading</p>
           </Link>
         </div>
-      </div>
-      <div id="middleStory" className="z-10 flex flex-col justify-center items-center align-top max-w-[25vw] bg-white bg-opacity-20 backdrop-blur-lg p-3 text-gray-600">
-        <h2>I had started programming at the age of 13. I started with VB.NET, then I migrated to...</h2>
-        <Link to="mystory" className="cursor-context-menu">
-          <p className="underline text-blue-600">Continue reading</p>
-        </Link>
-      </div>
-      <div id="topdiv" className="z-10 flex flex-col items-center align-top">
-        <div className="z-10 flex flex-row items-center align-top">
-          <h1 id="gigatitle" className="text-white animate-slideInFromTop">LUCA MONTANARI</h1><p id="gigatitlesub" className="text-5xl text-white animate-slideInFromTop">'S</p>
-        </div>
-        <h1 className="text-5xl text-white -mt-5 mb-5">Portfolio</h1>
-        <div className="z-10 flex align-middle items-center gap-5">
-          <div className='' id='downloadbtncontainer'>
-            <div className='flex justify-center pt-2'>
-              <div className='w-fit'>
-                <Link to="hireme" className="cursor-context-menu">
-                  <div className="downloadbtn_div">
-                    <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
-                      <div className='flex flex-col justify-center items-center'>
-                        <p className="text-blue-600">Hire Me</p>
-                      </div>
-                    </motion.button>
-                  </div>
-                </Link>
-              </div>
-            </div>
+        <div id="topdiv" className="z-10 flex flex-col items-center align-top">
+          <div className="z-10 flex flex-row items-center align-top">
+            <h1 id="gigatitle" className="text-white animate-slideInFromTop">LUCA MONTANARI</h1><p id="gigatitlesub" className="text-5xl text-white animate-slideInFromTop">'S</p>
           </div>
-          <img id="profilepic1" className="w-[10vw] h-[10vw] rounded-full border-2 border-gray-600 animate-reveal"></img>
-          <div className='' id='downloadbtncontainer'>
-            <div className='flex justify-center pt-2'>
-              <div className='w-fit'>
-                <Link to="contactme" className="cursor-context-menu">
-                  <div className="downloadbtn_div">
-                    <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
-                      <div className='flex flex-col justify-center items-center'>
-                        <p className="text-blue-600">Contact Me</p>
-                      </div>
-                    </motion.button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="fjc mt-3 mb-20 text-blue-300 underline text-lg"><a href="mailto:luca@lucasnode.com"><p>luca@lucasnode.com</p></a></div>
-      </div>
-
-      <img className="z-20 w-[28vw]" src={tertitimg}></img>
-      <div id="myinfodiv" className="z-10 -mt-2 flex flex-col justify-center items-center align-top w-[27.9vw] bg-black bg-opacity-40 backdrop-blur-lg p-3
-       text-gray-300">
-        <div className="flex flex-col items-left align-top">
-          <h2 className="text-4xl">
-            <span className="text-green-400">.age</span>&nbsp; <span className="text-purple-400">$&gt;</span> 18 years old
-            <br />
-            <span className="text-green-400">.loc</span> &nbsp;&nbsp;<span className="text-purple-400">$&gt;</span> Living in <span className="text-green-500">It</span><span className="text-white">a</span><span className="text-red-500">ly</span>
-            <br />
-            <span className="text-green-400">.lang</span> <span className="text-purple-400">$&gt;</span> Speaking en & it
-          </h2>
-        </div>
-      </div>
-      <div id="myinfodiv1" className="z-10 mt-10 flex flex-col justify-center items-center align-top w-[425px] bg-black bg-opacity-40 backdrop-blur-lg p-3
-       text-gray-300 rounded-md">
-        <h2 className="text-3xl">
-          <span className="text-purple-400">Current Profession &gt;</span> Student
-        </h2>
-      </div>
-      <div id="freelancingSectionDiv" className="z-10 mt-10 mb-20 flex flex-col justify-center items-center align-top w-[40vw] bg-white bg-opacity-10
-      backdrop-blur-md p-3 text-gray-300 rounded-lg">
-        <div className="flex flex-col items-center align-top mb-6">
-          <h1 className="text-4xl text-[#2dc590]">
-            Freelancing Services
-          </h1>
-        </div>
-        <div className="flex flex-col items-left align-top">
-          <h2 className="text-xl">
-            <span className="text-purple-400">~</span> front-end web development
-            <br />
-            <span className="text-purple-400">~</span> simple to advanced pc app development (macOS, Windows)
-          </h2>
-        </div>
-        <div className='mt-4' id='downloadbtncontainer'>
-          <div className='flex justify-center pt-2'>
-            <div className='w-fit'>
-              <Link to="buyservice" className="cursor-context-menu">
-                <div className="downloadbtn_div">
-                  <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
-                    <div className='flex flex-col justify-center items-center'>
-                      <p className="text-blue-600">Buy a service</p>
+          <h1 className="text-5xl text-white -mt-5 mb-5">Portfolio</h1>
+          <div className="z-10 flex align-middle items-center gap-5">
+            <div className='' id='downloadbtncontainer'>
+              <div className='flex justify-center pt-2'>
+                <div className='w-fit'>
+                  <Link to="hireme" className="cursor-context-menu">
+                    <div className="downloadbtn_div">
+                      <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
+                        <div className='flex flex-col justify-center items-center'>
+                          <p className="text-blue-600">Hire Me</p>
+                        </div>
+                      </motion.button>
                     </div>
-                  </motion.button>
+                  </Link>
                 </div>
-              </Link>
+              </div>
+            </div>
+            <img id="profilepic1" className="w-[10vw] h-[10vw] rounded-full border-2 border-gray-600 animate-reveal"></img>
+            <div className='' id='downloadbtncontainer'>
+              <div className='flex justify-center pt-2'>
+                <div className='w-fit'>
+                  <Link to="contactme" className="cursor-context-menu">
+                    <div className="downloadbtn_div">
+                      <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
+                        <div className='flex flex-col justify-center items-center'>
+                          <p className="text-blue-600">Contact Me</p>
+                        </div>
+                      </motion.button>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="fjc mt-3 mb-20 text-blue-300 underline text-lg"><a href="mailto:luca@lucasnode.com"><p>luca@lucasnode.com</p></a></div>
+        </div>
+
+        <img className="z-20 w-[28vw]" src={tertitimg}></img>
+        <div id="myinfodiv" className="z-10 -mt-2 flex flex-col justify-center items-center align-top w-[27.9vw] bg-black bg-opacity-40 backdrop-blur-lg p-3
+       text-gray-300">
+          <div className="flex flex-col items-left align-top">
+            <h2 className="text-4xl">
+              <span className="text-green-400">.age</span>&nbsp; <span className="text-purple-400">$&gt;</span> 18 years old
+              <br />
+              <span className="text-green-400">.loc</span> &nbsp;&nbsp;<span className="text-purple-400">$&gt;</span> Living in <span className="text-green-500">It</span><span className="text-white">a</span><span className="text-red-500">ly</span>
+              <br />
+              <span className="text-green-400">.lang</span> <span className="text-purple-400">$&gt;</span> Speaking en & it
+            </h2>
+          </div>
+        </div>
+        <div id="myinfodiv1" className="z-10 mt-10 flex flex-col justify-center items-center align-top w-[425px] bg-black bg-opacity-40 backdrop-blur-lg p-3
+       text-gray-300 rounded-md">
+          <h2 className="text-3xl">
+            <span className="text-purple-400">Current Profession &gt;</span> Student
+          </h2>
+        </div>
+        <div id="freelancingSectionDiv" className="z-10 mt-10 mb-10 flex flex-col justify-center items-center align-top w-[40vw] bg-white bg-opacity-10
+      backdrop-blur-md p-3 text-gray-300 rounded-lg">
+          <div className="flex flex-col items-center align-top mb-6">
+            <h1 className="text-4xl text-[#2dc590]">
+              Freelancing Services
+            </h1>
+          </div>
+          <div className="flex flex-col items-left align-top">
+            <h2 className="text-xl">
+              <span className="text-purple-400">~</span> front-end web development
+              <br />
+              <span className="text-purple-400">~</span> simple to advanced pc app development (macOS, Windows)
+            </h2>
+          </div>
+          <div className='mt-4' id='downloadbtncontainer'>
+            <div className='flex justify-center pt-2'>
+              <div className='w-fit'>
+                <Link to="buyservice" className="cursor-context-menu">
+                  <div className="downloadbtn_div">
+                    <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
+                      <div className='flex flex-col justify-center items-center'>
+                        <p className="text-blue-600">Buy a service</p>
+                      </div>
+                    </motion.button>
+                  </div>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="z-10 fixed text-left bottom-3 left-3 opacity-70 text-white">
-        <p>Make sure to disable any darkmode extension like Noir.</p>
-      </div>
-      <div className="z-10 fixed text-left bottom-3 right-3 opacity-70 text-white">
-        <p>All copyright® rights reserved to Luca Montanari.</p>
-      </div>
-
-      <div id="bottomLinks" className="z-10 fixed bottom-0 flex flex-row text-center text-white bg-gray-700 bg-opacity-95 py-2 px-5">
-        <div className="flex flex-row gap-2 align-midde text-white">
-          <a href="https://is.gd/VuJcew" className="mt-[5px]">
-            <FaYoutube className="h-8 w-8 hover:text-green-500" />
-          </a>
-          <RxDividerVertical className="w-8 h-8 mt-1" />
-          <a href="https://is.gd/flpzrg" className="mt-[5px]">
-            <FaInstagram className="w-8 h-8 hover:text-green-500" />
-          </a>
+        <div id="freelancingSectionDiv" className="z-10 mb-20 flex flex-col justify-center items-center align-top w-[40vw] bg-white bg-opacity-10
+      backdrop-blur-md p-3 text-gray-300 rounded-lg">
+          <div className="flex flex-col items-center align-top">
+            <h1 className="text-4xl text-[#2dc590]">
+              Support my projects
+            </h1>
+          </div>
+          <div className='mt-4' id='downloadbtncontainer'>
+            <div className='flex justify-center pt-2'>
+              <div className='w-fit'>
+                <Link to="donate" className="cursor-context-menu">
+                  <div className="downloadbtn_div">
+                    <motion.button className="downloadbtn" whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
+                      <div className='flex flex-col justify-center items-center'>
+                        <p className="text-blue-600">Donate</p>
+                      </div>
+                    </motion.button>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div >
-  );
-}
+
+        <div className="z-10 fixed text-left bottom-3 left-3 opacity-70 text-white">
+          <p>Make sure to disable any darkmode extension like Noir.</p>
+        </div>
+        <div className="z-10 fixed text-left bottom-3 right-3 opacity-70 text-white">
+          <p>All copyright® rights reserved to Luca Montanari.</p>
+        </div>
+
+        <div id="bottomLinks" className="z-10 fixed bottom-0 flex flex-row text-center text-white bg-gray-700 bg-opacity-95 py-2 px-5">
+          <div className="flex flex-row gap-2 align-midde text-white">
+            <a href="https://is.gd/VuJcew" className="mt-[5px]">
+              <FaYoutube className="h-8 w-8 hover:text-green-500" />
+            </a>
+            <RxDividerVertical className="w-8 h-8 mt-1" />
+            <a href="https://is.gd/flpzrg" className="mt-[5px]">
+              <FaInstagram className="w-8 h-8 hover:text-green-500" />
+            </a>
+          </div>
+        </div>
+      </div >
+    );
+  }
+
+// function glitchEffect() {
+//       return (
+//         <div className="glitchText">
+//           <h1 data-value="TEST" id="glitchText">TEST</h1>
+//         </div>
+//       );
+//     }
---
+++ app/routes/donate.tsx
@@ -0,0 +1,34 @@
+import type { MetaFunction } from "@remix-run/node";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+import "../styles/global.css";
+
+export default function DonatePage() {
+
+  useEffect(() => {
+    console.log("Page loaded.");
+  });
+
+  return (
+    <div className="flex flex-col align-top darkmode-bg" style={{ width: "100vw", height: "100vh" }}>
+      <input type="text" className="absolute hidden" id="pageName" value={"donate"} readOnly></input>
+      <div style={{ width: "100%", height: "auto" }} className="flex flex-col items-center align-top bg-white dark:bg-[#1E1E1E] text-white">
+        <div id="topdiv" className="z-10 flex flex-col align-top items-center mt-20">
+          <h1 id="title" className="text-gray-500 animate-slideInFromTop">Donate to Luca</h1>
+        </div>
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
+          <input type="hidden" name="hosted_button_id" value="AET2KMM66MX96" />
+          <motion.button name="submit" className="w-[200px] h-[50px] bg-gray-500 rounded-lg font-sans border-black dark:border-white border-2"
+            whileHover={{ scale: 1.07 }} whileTap={{ scale: 0.8 }}>
+            <div className='flex flex-col justify-center items-center'>
+              <p>DONATE</p>
+            </div>
+          </motion.button>
+          {/* <input type="image" src="https://pics.paypal.com/00/s/YWM5ODNjYTItZDMxMC00NDdlLThkMGQtOTg0N2VjMzcyYzI1/file.JPG" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /> */}
+          <img alt="" src="https://www.paypal.com/en_IT/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+      </div>
+    </div>
+  );
+}
