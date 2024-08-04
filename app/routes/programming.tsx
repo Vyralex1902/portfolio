710d4c3a029bea038ee98f4178dca157a92f9113
Cyberspark <97030842+Vyralex1902@users.noreply.github.com>
2024-08-04 09:30:03 +0200

    Update programming.tsx

--- app/routes/programming.tsx
+++ app/routes/programming.tsx
@@ -46,7 +46,7 @@ export default function Programming() {
               is the total amount of years of experience I have with that specific language. <br />y = years; m = months
             </p>
             <p>
-              <span className="text-purple-400">&gt;</span> Python <span className="text-cyan-800 dark:text-cyan-500">[<1m]</span><br />
+              <span className="text-purple-400">&gt;</span> Python <span className="text-cyan-800 dark:text-cyan-500">[&lt;1m]</span><br />
               <span className="text-purple-400">&gt;</span> JS & TS <span className="text-cyan-800 dark:text-cyan-500">[4m]</span><br />
               <span className="text-purple-400">&gt;</span> HTML <span className="text-cyan-800 dark:text-cyan-500">[4m]</span><br />
               <span className="text-purple-400">&gt;</span> CSS <span className="text-cyan-800 dark:text-cyan-500">[4m]</span><br />
